d31ca3818b81c10a6d6699a47f54aa109637ffcd
BizPlug <166514136+BizPlug@users.noreply.github.com>
2024-04-13 17:19:32 +0200

    Update config.php

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,9 +1,9 @@
 <?php
 
 	// Establish database connection (replace with your actual credentials)
-	$host = 'localhost';
-	$user = 'root';
-	$pass = '';
-	$db = 'bizplug';
+	$host = 'sql.freedb.tech';
+	$user = 'freedb_root_admin';
+	$pass = '*2NXeAFUReWv%P3';
+	$db = 'freedb_bizplug';
 
 ?>
